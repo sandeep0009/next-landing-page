da28e71072eeccbcccec0b63e493a242088cb4b0
sandeep0009 <500sandeepks@gmail.com>
2024-07-05 19:26:51 +0530

    completed the navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,10 @@
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <h1>project setup</h1>
+    <h1>
+      <Navbar/>
+    </h1>
     
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,56 @@
 import React from 'react'
+import Link from 'next/link'
+import { Button } from './ui/button'
+import { ArrowRight } from 'lucide-react'
 
+const links=[
+    {
+        name:"Home",
+        path:'/'
+    },
+    {
+        name:'Features',
+        path:'/features'
+    },
+    {
+        name:"Community",
+        path:'/community'
+    },
+    {
+        name:'Blogs',
+        path:'/blogs'
+    },
+    {
+        name:'Pricing',
+        path:'/prices'
+    }
+]
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="container">
+        <div className="container flex justify-between py-4">
+            <div className="container">
+                <div className="heading">
+                    <h2 className="font-bold">NexCent</h2>
+                </div>
+
+            </div>
+
+            <div className=" hidden lg:flex items-center">
+               
+                <ul className="flex px-4">
+            {links.map((link) => (
+              <li key={link.path}>
+                <Link className="px-4" href={link.path}>{link.name}</Link>
+              </li>
+            ))}
+          </ul>
+               
+          <Button className='bg-green-500'>Register Now <ArrowRight/></Button>
+            </div>
+
+        </div>
+    </div>
   )
 }
 
