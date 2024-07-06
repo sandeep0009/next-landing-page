86490a1049fb729fca2b629ff47f3b8ad85e8aad
sandeep0009 <500sandeepks@gmail.com>
2024-07-06 19:54:13 +0530

    done with banner 2

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import Banner from "@/components/Banner";
+import Banner2 from "@/components/Banner2";
 import Card from "@/components/Card";
 import Client from "@/components/Client";
 import Hero from "@/components/Hero";
@@ -22,6 +23,7 @@ export default function Home() {
             <Manage/>
             <Testimonial/>
             <Card/>
+            <Banner2/>
 
     </div>
   
--- src/components/Banner2.jsx
+++ src/components/Banner2.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Button } from './ui/button'
+import { ArrowRight } from 'lucide-react'
 
 const Banner2 = () => {
   return (
-    <div>Banner2</div>
+    <div className="container m-auto text-center flex flex-wrap justify-center">
+      <div className="heading">
+        <h2 className="text-4xl font-bold mt-20">Pellentesque suscipit  <br />fringilla libero eu.</h2>
+        <Button className="bg-green-500 mt-7">Get a Demo <ArrowRight/></Button>
+
+      </div>
+
+    </div>
   )
 }
 
